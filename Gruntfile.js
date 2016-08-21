66c3742ba3081f9be626c0367a3b298db53844de
przemek@empressia.pl <przemek@empressia.pl>
2016-08-21 21:52:57 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded'
+                },
+                src: 'assets/scss/style.scss',
+                dest: 'assets/css/style.css'
+            }
+        },
+        
+        watch : {
+            html : {
+                files   : 'index.html',
+                options : {
+                    livereload : true
+                }
+            },
+            css : {
+                files   : 'assets/scss/**/*.scss',
+                tasks   : ['sass'],
+                options : {
+                    livereload : true
+                }
+            },
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // rejestrowanie domyślnego zestawu zadań dla Grunt.js
+    grunt.registerTask('default', ['watch']);
+};
