1206497166850aec3bf434e78e7588fc9b427848
przemek@empressia.pl <przemek@empressia.pl>
2016-08-23 05:33:03 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,41 +2,7 @@
 ( function ($) {
     'use strict';
     $(document).ready(function () {
-        //
-        //     $(window).scroll(function () {
-        //         var scroll = $(window).scrollTop();
-        //
-        //         if (scroll >= 97) {
-        //             $('body').addClass('scroll');
-        //             $('.navbar').addClass('scroll');
-        //         } else {
-        //             $('body').removeClass('scroll');
-        //             $('.navbar').removeClass('scroll');
-        //         }
-        //
-        //         if (scroll <= 0) {
-        //             $('#mainNav li a').parent().siblings().removeClass('active')
-        //             $('#mainNav li:first-child').addClass('active');
-        //         }
-        //     });
-        //
-        //     $('#scroll-to-top').click(function () {
-        //         $('body,html').animate({
-        //             scrollTop: 0
-        //         }, 500);
-        //     });
-        //     $('#mainNav li:first-child').addClass('active');
-        //
-        //     $('#mainNav li a').click(function () {
-        //         $(this).parent().siblings().removeClass('active')
-        //         $(this).parent().addClass('active');
-        //     });
-        //
-        //     $('.scroll').smoothScroll({
-        //         offset: -78,
-        //         speed: 1200
-        //     });
-        //
+
 
 
         $('.owl-carousel').owlCarousel({
@@ -59,23 +25,23 @@
         });
 
         $('.owl-carousel2').owlCarousel({
-            stagePadding: 150,
-            loop:false,
-            startPosition: 0,
+            itemsCustom : [
+                [0, 2],
+                [450, 4],
+                [600, 7],
+                [700, 9],
+                [1000, 10],
+                [1200, 12],
+                [1400, 13],
+                [1600, 15]
+            ],
             margin:20,
-            responsive:{
-                0:{
-                    items:1
-                },
-                600:{
-                    items:1
-                },
-                1000:{
-                    items:1
-                }
-            }
+            loop: false
         });
-        $('#fullpage').fullpage({
+        $('#fullpage').fullpage({});
+
+        $('.scroll').click(function(){
+            $.fn.fullpage.moveSectionDown();
         });
     });
 
