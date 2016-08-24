57c95454de2dd911f6e44990f77dca56d977e0ad
przemek@empressia.pl <przemek@empressia.pl>
2016-08-24 02:41:07 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,69 @@
-// jQuery.noConflict();
+jQuery.noConflict();
 ( function ($) {
     'use strict';
     $(document).ready(function () {
 
-
-
         $('.owl-carousel').owlCarousel({
             stagePadding: 200,
-            loop:true,
-            margin:0,
-            nav:true,
-            navText: ["<i class='fa fa-long-arrow-left' aria-hidden='true'></i>","<i class='fa fa-long-arrow-right' aria-hidden='true'></i>"],
-            responsive:{
-                0:{
-                    items:1
+            loop: true,
+            margin: 0,
+            nav: true,
+            navText: ["<i class='fa fa-long-arrow-left' aria-hidden='true'></i>", "<i class='fa fa-long-arrow-right' aria-hidden='true'></i>"],
+            responsive: {
+                0: {
+                    items: 1,
+                    stagePadding: 80
                 },
-                600:{
-                    items:1
+                768: {
+                    items: 1
                 },
-                1000:{
-                    items:1
+                992: {
+                    items: 1
                 }
             }
         });
 
         $('.owl-carousel2').owlCarousel({
-            itemsCustom : [
+            itemsCustom: [
                 [0, 2],
                 [450, 4],
                 [600, 7],
-                [700, 9],
-                [1000, 10],
+                [500, 9],
+                [500, 10],
                 [1200, 12],
                 [1400, 13],
                 [1600, 15]
             ],
-            margin:20,
+            margin: 20,
             loop: false
         });
         $('#fullpage').fullpage({});
-
-        $('.scroll').click(function(){
+        
+        $('.scroll').click(function () {
             $.fn.fullpage.moveSectionDown();
         });
-    });
 
+        $("#popup-video").click(function () {
+            $("#video-popup").fadeIn(500);
+            $.fn.fullpage.setAllowScrolling(false);
+            $.fn.fullpage.setKeyboardScrolling(false);
+        });
+        $("#close-btn-video").click(function () {
+            $("#video-popup").fadeOut(500);
+            $.fn.fullpage.setAllowScrolling(true);
+            $.fn.fullpage.setKeyboardScrolling(true);
+        });
+
+        $("#popup-form").click(function () {
+            $("#form-popup").fadeIn(500);
+            $.fn.fullpage.setAllowScrolling(false);
+            $.fn.fullpage.setKeyboardScrolling(false);
+        });
+        $("#close-btn-form").click(function () {
+            $("#form-popup").fadeOut(500);
+            $.fn.fullpage.setAllowScrolling(true);
+            $.fn.fullpage.setKeyboardScrolling(true);
+        });
+        
+    });
 }(jQuery));
